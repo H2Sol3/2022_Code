524fc6f93ca2625dc8fb6710032b95ad5a5097d8
thfastang401 <thfastang401@naver.com>
2022-07-15 22:41:18 +0900

    로또 하다가 멈추기..다시 코드 짜보기

---
+++ 22.04 CodingTest/src/Test_07/Lotto.java
@@ -0,0 +1,16 @@
+package Test_07;
+//로또번호생성기
+public class Lotto {
+
+	public static void main(String[] args) {
+		System.out.println("로또번호 생성해서 부자되기");
+		int[] num = new int[45];
+		int[] choice = new int[6];
+		
+		for(int i = 1; i<=num.length; i++) {
+			
+
+		}
+	}
+
+}
